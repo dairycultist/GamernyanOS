0f25374bf8dd8f97aa569a6b453284b67c54695d
cultist <75032462+dairycultist@users.noreply.github.com>
2025-08-09 01:13:08 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <avr/io.h>
+
+typedef unsigned char byte;
+
+#include "uart.c"
+
+int main() {
+
+	uart_init();
+
+	// CLI
+	byte buffer[256] = {0};
+	int cursor = 0;
+	byte in;
+
+	uart_putstring("> ");
+
+	while (1) {
+
+		in = uart_getbyte();
+
+		if (in == 127) { // delete
+
+			if (cursor) {
+				buffer[--cursor] = '\0';
+				uart_putstring("\b \b");
+			}
+
+		} else if (in == '\r') { // enter
+
+			uart_putbyte('\n');
+			uart_putstring(buffer);
+			uart_putstring("\n> ");
+
+			while (cursor) {
+				buffer[--cursor] = '\0';
+			}
+
+		} else if (in == 27) { // ansii escape codes
+
+			// cursor up down left right are \[cA - \[cD
+			uart_putbyte('\\');
+
+		} else {
+
+			buffer[cursor++] = in;
+			uart_putbyte(in);
+		}
+	}
+
+    return 0;
+}
---
+++ uart.c
@@ -0,0 +1,31 @@
+#define F_CPU 16000000UL
+#define BAUD 9600
+#define BAUD_RATE ((F_CPU / (16UL * BAUD)) - 1)
+
+// initialize UART (serial communication)
+void uart_init() {
+
+    UBRR0H = (byte) (BAUD_RATE >> 8);
+    UBRR0L = (byte) BAUD_RATE;
+
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // set frame format: 8 data bits, 1 stop bit, no parity
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   // enable receiver (RX) and transmitter (TX)
+}
+
+void uart_putbyte(byte data) {
+
+    while (!(UCSR0A & _BV(UDRE0)));	// wait for empty register
+    UDR0 = data;					// put data into register (queuing it for transmission)
+}
+
+void uart_putstring(byte *data) {
+
+	while (*data)
+		uart_putbyte(*data++);
+}
+
+byte uart_getbyte() {
+
+	while (!(UCSR0A & _BV(RXC0)));	// wait until data exists
+    return UDR0;					// return data
+}
